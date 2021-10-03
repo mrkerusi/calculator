2997b5c3fd451f108e7638ca1f863bfe925d5aa6
Brightson Jupiri <58578547+mrkerusi@users.noreply.github.com>
2021-10-04 02:45:42 +0800

    Added comment to explain addition
    Code is efficient, but confusing.

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -9,9 +9,9 @@ exports.calculate = function(req, res) {
     res.status(400);
     res.json({ error: err.message });
   });
-
+  //Using + Operator to coerce variables to numbers to void string concatenation.
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
